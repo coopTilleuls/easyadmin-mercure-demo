9280424f84b56d2738658646f8a1ed6b043cb269
Loïc Vernet <loic@les-tilleuls.coop>
2023-05-25 16:32:42 +0200

    mercure: manual notification example

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Dto\RegisterFormDto;
+use App\Entity\Article;
 use App\Form\Type\RegisterForm;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -19,12 +23,32 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(name: 'app_')]
 final class AppController extends AbstractController
 {
+    public function __construct(
+        public readonly AdminUrlGenerator $adminUrlGenerator,
+        public readonly HubInterface $hub,
+    ) {
+    }
+
     /**
      * Simple page with some dynamic content.
      */
     #[Route(path: '/', name: 'home')]
     public function home(): Response
     {
+        // Send an update manually to Mercure: for example, a user has updated one
+        // of a their article and an administrator it currently editing the article
+        // for exemple this code could be used in a Doctrine listener when an entity
+        // get updated.
+        $topic = $this->adminUrlGenerator->setController(Article::class)
+                ->unsetAllExcept('crudControllerFqcn')->generateUrl();
+        /** @noinspection JsonEncodingApiUsageInspection */
+        $update = new Update(
+            $topic,
+            (string) json_encode(['id' => 1]),
+        );
+
+        $this->hub->publish($update);
+
         $readme = file_get_contents(__DIR__.'/../../README.md');
 
         return $this->render('home.html.twig', compact('readme'));
