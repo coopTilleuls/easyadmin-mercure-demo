affac75ced1b99114ab6155c923a88e7e7424af8
Loïc Vernet <loic@les-tilleuls.coop>
2023-06-01 17:33:27 +0200

    cs: commented the hub class

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -12,8 +12,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\Mercure\HubInterface;
-use Symfony\Component\Mercure\Update;
+// use Symfony\Component\Mercure\HubInterface;
+// use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -25,7 +25,7 @@ final class AppController extends AbstractController
 {
     public function __construct(
         public readonly AdminUrlGenerator $adminUrlGenerator,
-        public readonly HubInterface $hub,
+        // public readonly HubInterface $hub,
     ) {
     }
 
