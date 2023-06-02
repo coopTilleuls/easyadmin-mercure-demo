054779415d1688bb6446e156554dea51630ff4e8
Loïc Vernet <loic@les-tilleuls.coop>
2023-06-02 17:00:47 +0200

    tests: 100% cov

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ORM\Entity]
 class Article
 {
     #[ORM\Id]
